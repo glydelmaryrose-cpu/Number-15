6e03d14434d241f87fa1b30b6295b45e4e6cf269
Glydel Mary Rose Visto <glydelmaryrose@gmail.com>
2025-12-07 03:47:46 +0800

    Update Nunber15.java

--- Nunber15.java
+++ Nunber15.java
@@ -9,23 +9,27 @@ import java.util.*;
 public class Nunber15
 {
    public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       int[] arr = new int[5];
-       for (int i = 0; i < 5; i++) {
-           System.out.print("Enter number " + (i + 1) + "; ");
-           arr[i] = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[5];
+        
+        for (int a = 0; a < 5; a++) {
+            System.out.print("Enter number " + (a + 1) + ": ");
+            arr[a] = scanner.nextInt();
         }
-        System.out.println("Numbers greater than 50: ");
+        System.out.println("Numbers greater than 50:");
+        boolean found = false;  
+        
         for (int num : arr) {
             if (num > 50) {
                 System.out.print(num + " ");
-                
+                found = true; 
             }
         }
+        if (!found) {
+            System.out.println("None of the numbers are greater than 50.");
+        } else {
+            System.out.println(); 
+        }
         scanner.close();
-       }
+    }
    }
-
-   
-   
-   
